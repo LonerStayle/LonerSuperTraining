c92fe08815b393a52a7b56439f51185f51a58a3a
Loner123 <dlwlstjq410@gmail.com>
2020-10-16 01:51:12 +0900

    [ADD] Splash 테마 이용해서 적용.

--- app/src/main/java/com/example/supertraining/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/supertraining/view/activity/MainActivity.kt
@@ -2,19 +2,37 @@ package com.example.supertraining.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.databinding.DataBindingUtil
 import com.example.supertraining.R
 import com.example.supertraining.databinding.ActivityMainBinding
+import com.example.supertraining.view.utill.tedPermissionCheck
+import com.example.supertraining.view.utill.toastShortShow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).run {
+        Thread.sleep(1000L)
+            setTheme(R.style.AppTheme)
 
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this@MainActivity,
+            R.layout.activity_main
+        ).run {
+            tedPermissionCheck(this@MainActivity) { toastShortShow("ㅎㅇ") }
             root
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
 }
